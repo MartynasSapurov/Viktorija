081937397822916d6d7841cecfa182875f7fc749
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-01-23 15:23:17 +0200

    Create print.py

---
+++ print.py
@@ -0,0 +1,5 @@
+#https://www.geeksforgeeks.org/formatted-string-literals-f-strings-python/
+name = input("Įveskite savo vardą ")
+average = input("Įveskite savo pažymių vidurkį")
+print("Jūsų vardas yra", name, ", o pažymių vidyrkis yra", average)
+print(f"Jūsų vardas yra {name}, o pažymių vidyrkis yra {average}")
