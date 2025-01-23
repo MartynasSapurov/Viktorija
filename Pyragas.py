a54a19879e655b79d914c0d212bfac3557c3a2f0
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-01-23 16:14:15 +0200

    Update Pyragas.py

--- Pyragas.py
+++ Pyragas.py
@@ -1,12 +1,12 @@
 kg_kaina = 7.55
 perkame_gramu = 700
-pyrago_kg_kaina = float(input("Įveskite pyrago kainą"))
-pyrago_svoris = float(input("Įveskite pyrago svorį"))
+pyrago_kg_kaina = float(input("Įveskite pyrago kainą\n"))
+pyrago_svoris = float(input("Įveskite pyrago svorį\n"))
 
 salainiu_kaina = kg_kaina * 700/1000
 pyrago_kaina = pyrago_kg_kaina * pyrago_svoris
 pirkinio_kaina = salainiu_kaina + pyrago_kaina
 
 print(f"Saldainių kaina: {salainiu_kaina} Eur")
-print(f"Pirkinio kaina: {pyrago_kaina} Eur")
-print(f"Saldainių kaina: {pirkinio_kaina} Eur")
+print(f"Pyrago kaina: {pyrago_kaina} Eur")
+print(f"Pirkinio kaina: {pirkinio_kaina} Eur")
